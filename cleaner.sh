1083531e444321f6c3970d3b02c58f5c819637b7
mrbrownstone07 <mahdi.mad07@gmail.com>
2018-12-01 17:36:08 +0600

    updated the cleaner

--- cleaner.sh
+++ cleaner.sh
@@ -1,4 +1,22 @@
-!#/bin/bash 
+#!/bin/bash
 
+cd /home/m/Code/assembler/
 
-find ./ -name "*.txt" | grep -v "validtc.txt|invalidtc.txt|validtc.txt|about.txt|hexHelper.txt|hexHelper.txt|oppcode.txt|regMap.txt" | xargs rm
+# Exit if the directory isn't found.
+if (($?>0)); then
+    echo "Can't find work dir... exiting"
+    exit
+fi
+
+for i in *; do
+    if ! grep -qxFe "$i" filelist.txt; then
+        echo ">> Enter 0 to delete: $i"
+        read arg
+        
+        if (($arg == 0 )); then
+        echo ">> Deleted: $i"
+        rm "$i"
+        fi
+    fi
+done
+
